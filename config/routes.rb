8bced28b9138d6bdb6558efb4bacded758fd44bc
prativa <devkotaprativa@gmail.com>
2015-11-02 19:08:02 +0545

    root path for authenticated and unauthenticated user is defined in routes file. Footer added to the application.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,13 @@ Rails.application.routes.draw do
   devise_for :users
 
   resource :socials
-  root 'socials#homepage'
+
+  devise_scope :user do
+    authenticated :user do
+      root :to => 'socials#index'
+    end
+    unauthenticated :user do
+      root :to => 'socials#homepage', as: :unauthenticated_root
+    end
+  end
 end
